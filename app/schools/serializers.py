7c6793fe7efc623ce9ba25a6413cfb3ee6da9f96
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2023-01-16 22:12:08 +0300

    initializing Schools model

---
+++ app/schools/serializers.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework import serializers
+from .models import School
+
+
+class SchoolCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    DP = serializers.CharField(read_only=True)
+
+    def validate(self, data, *args, **kwargs):
+       return super(UserCreateSerializer, self).validate(data, *args, **kwargs)
+
+    def create(self, validated_data):
+        # Register new users
+        user = super(UserCreateSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+    class Meta:
+        model = School
+        fields = ('email', 'id', 'password', 'username', 'role', 'DoB', 'contact', 'residence', 'gender', 'country', 'DP')
+        extra_kwargs = {'password':{'write_only':True}}
+
+class SchoolUpdateSerializer(serializers.ModelSerializer):
+
+    def validate(self, data, *args, **kwargs):
+       return super(UserUpdateSerializer, self).validate(data, *args, **kwargs)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data['email']
+        instance.username = validated_data['username']
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance.role = validated_data['role']
+        instance.DoB = validated_data['DoB']
+        instance.contact = validated_data['contact']
+        instance.residence = validated_data['residence']
+        instance.gender = validated_data['gender']
+        instance.country = validated_data['country']
+        instance.save()
+        return instance
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name', 'role', 'DoB', 'contact', 'residence', 'gender', 'country')
+
+class SchoolListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'role', 'DoB', 'contact', 'residence', 'gender', 'country', 'DP')
---
+++ app/schools/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    ListSchoolsView,
+    GetSchoolView,
+    AddSchoolView,
+    UpdateSchoolView,
+    SchoolsDPView,
+)
+
+app_name = 'schools'
+
+urlpatterns = [
+    path('', ListSchoolsView.as_view(), name='list_schools'),
+    path('<int:school_id>/', GetSchoolView.as_view(), name='get_school'),
+    path('register/', AddSchoolView.as_view(), name='register_school'),
+    path('<int:school_id>/update/', UpdateSchoolView.as_view(), name='update_school'),
+    path('<int:school_id>/dp-upload/', SchoolsDPView.as_view(), name='school_dp_upload'),
+]
