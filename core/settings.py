6550707b525e40645b065404af74a6fd4668372e
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2023-01-17 16:50:29 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '6%hc3+^4=3phyx=86dqp%kl!g850jsv1keaw4u7tv(b&k_d324'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "shulehubAPI.pythonanywhere.com"]
 
 
 # Application definition
