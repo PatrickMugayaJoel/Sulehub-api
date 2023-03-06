c60e82af01e5ce3bf1f4673cc6a684f0ac39163a
Mugaya Joel Patrick <patrickthemajesty@gmail.com>
2023-03-06 11:38:56 +0300

    Update admin.py

--- apps/study_groups/admin.py
+++ apps/study_groups/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
-# from .models import StudyGroup
+from django.contrib import admin
+from .models import StudyGroup
 
-# admin.site.register(StudyGroup)
+admin.site.register(StudyGroup)
